5de18c64dabcb4d37a8bf16ad28cce79b053a84e
nvchernov <q-cs@ya.ru>
2018-02-16 21:26:51 +0300

    new methods to check if string is blank or present

--- NSupport/StringAccess.cs
+++ NSupport/StringAccess.cs
@@ -1,17 +1,20 @@
-﻿namespace NSupport {
+﻿namespace NSupport
+{
     using System;
 
     /// <summary>
     /// Provide access methods for <see cref="string"/>.
     /// </summary>
-    public static class StringAccess {
+    public static class StringAccess
+    {
 
         /// <summary>
         /// Check whether given <see cref="string"/> is null or empty.
         /// </summary>
         /// <param name="source">An instance of <see cref="string"/>.</param>
         /// <returns>true if <paramref name="source"/> is null or empty, otherwise false.</returns>
-        public static bool IsNullOrEmpty(this string source) {
+        public static bool IsNullOrEmpty(this string source)
+        {
             return string.IsNullOrEmpty(source);
         }
 
@@ -20,16 +23,29 @@
         /// </summary>
         /// <param name="source">An instance of <see cref="string"/>.</param>
         /// <returns>true if <paramref name="source"/> is null or empty or only whitespace, otherwise false.</returns>
-        public static bool IsBlank(this string source) {
+        public static bool IsBlank(this string source)
+        {
             return string.IsNullOrWhiteSpace(source);
         }
 
+        /// <summary>
+        /// Check whether given <see cref="string"/> is not null or empty or only whitespace.
+        /// </summary>
+        /// <param name="source">An instance of <see cref="string"/>.</param>
+        /// <returns>true if <paramref name="source"/> is null or empty or only whitespace, otherwise false.</returns>
+        public static bool IsPresent(this string source)
+        {
+            return !string.IsNullOrWhiteSpace(source);
+        }
+
+        // TODO make namesapce (like NSupport.Neworking) for this type of operations
         /// <summary>
         /// Encode string as url string.
         /// </summary>
         /// <param name="source"><see cref="string"/> instance to encode.</param>
         /// <returns>Encoded url string.</returns>
-        public static string UrlEncode(this string source) {
+        public static string UrlEncode(this string source)
+        {
             Guard.ArgumentNotNull("source", source);
 
             return Uri.EscapeDataString(source);
@@ -40,10 +56,13 @@
         /// </summary>
         /// <param name="source"><see cref="string"/> instance to decode.</param>
         /// <returns>Original string of url string.</returns>
-        public static string UrlDecode(this string source) {
+        public static string UrlDecode(this string source)
+        {
             Guard.ArgumentNotNull("source", source);
 
             return Uri.UnescapeDataString(source.Replace("+", "%20"));
         }
+
+
     }
 }
