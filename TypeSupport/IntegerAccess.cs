e8b7c30cef7b1de470b1217f850707629d6df543
nvchernov <q-cs@ya.ru>
2018-03-23 23:57:05 +0300

    Integer methods refactoring

--- TypeSupport/IntegerAccess.cs
+++ TypeSupport/IntegerAccess.cs
@@ -1,20 +1,24 @@
-﻿namespace NSupport {    
+﻿namespace NSupport
+{
     using System;
 
     /// <summary>
     /// Provides extension methods for <see cref="int"/> for easy access.
     /// </summary>
-    public static class IntegerAccess {
-        private static Action emptyAction = (() => { });
-        private static Action<int> emptyIndexAction = ((i) => { });
-
+    public static class IntegerAccess
+    {
         /// <summary>
         /// Loops the current <see cref="int" /> times with the given <paramref name="action"/>.
         /// </summary>
         /// <param name="source">A <see cref="int"/> instance.</param>
         /// <param name="action">A <see cref="Action" /> to loop.</param>
-        public static void Times(this int source, Action action) {            
-            source.Times((i) => { (action ?? emptyAction)(); });
+        public static void Times(this int source, Action action)
+        {
+            if (action == null)
+                return;
+
+            for (int i = 0; i < source; i++)
+                action();
         }
 
         /// <summary>
@@ -22,10 +26,13 @@
         /// </summary>
         /// <param name="source">A <see cref="int"/> instance.</param>
         /// <param name="action">A <see cref="Action{T}" /> to loop.</param>
-        public static void Times(this int source, Action<int> action) {            
-            for (int i = 0; i < source; i++) {
-                (action ?? emptyIndexAction)(i);
-            }
+        public static void Times(this int source, Action<int> action)
+        {
+            if (action == null)
+                return;
+
+            for (int i = 0; i < source; i++)
+                action(i);
         }
 
         /// <summary>
@@ -33,28 +40,40 @@
         /// </summary>
         /// <param name="source">An instance of <see cref="int"/>.</param>
         /// <returns></returns>
-        public static bool IsBlank(this int? source) => source == null || source == default(int);
+        public static bool IsBlank(this int? source)
+        {
+            return source == null || source == default(int);
+        }
 
         /// <summary>
         /// Check if <paramref name="source"/> is not null and has any element.
         /// </summary>
         /// <param name="source">An instance of <see cref="int"/>.</param>
         /// <returns></returns>
-        public static bool IsPresent(this int? source) => source != null && source != default(int);
+        public static bool IsPresent(this int? source)
+        {
+            return source != null && source != default(int);
+        }
 
         /// <summary>
         /// Check if <paramref name="source"/> is null or has no elements.
         /// </summary>
         /// <param name="source">An instance of <see cref="int"/>.</param>
         /// <returns></returns>
-        public static bool IsBlank(this int source) => source == default(int);
+        public static bool IsBlank(this int source)
+        {
+            return source == default(int);
+        }
 
         /// <summary>
         /// Check if <paramref name="source"/> is not null and has any element.
         /// </summary>
         /// <param name="source">An instance of <see cref="int"/>.</param>
         /// <returns></returns>
-        public static bool IsPresent(this int source) => source != default(int);
+        public static bool IsPresent(this int source)
+        {
+            return source != default(int);
+        }
 
         /// <summary>
         /// Check if int is inside closed interval
