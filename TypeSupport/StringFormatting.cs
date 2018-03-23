60b57dc08d5c5078f9191744e4b4c4cf5253920a
nvchernov <q-cs@ya.ru>
2018-03-24 00:00:44 +0300

    string methods refactoring

--- TypeSupport/StringFormatting.cs
+++ TypeSupport/StringFormatting.cs
@@ -1,11 +1,13 @@
-﻿namespace NSupport {
+﻿namespace NSupport
+{
     using System;
     using System.Threading;
 
     /// <summary>
     /// Provides extension methods for <see cref="string"/> for fomatting.
     /// </summary>
-    public static class StringFormatting {
+    public static class StringFormatting
+    {
         /// <summary>
         /// Replaces one or more format items in a specified string with the string representation of a specified object.
         /// This method uses <see cref="String.Format(string, object[])"/> method intenally.
@@ -13,7 +15,8 @@
         /// <param name="source">A <see cref="string"/> instance.</param>
         /// <param name="args">An object array that contains zero or more objects to format. </param>
         /// <returns>A copy of <paramref name="source"/> in which the format items have been replaced by the string representation of the corresponding objects in <paramref name="args"/>.</returns>
-        public static string FormatWith(this string source, params object[] args) {
+        public static string FormatWith(this string source, params object[] args)
+        {
             return source.FormatWith(Thread.CurrentThread.CurrentCulture, args);
         }
 
@@ -25,7 +28,8 @@
         /// <param name="provider">An object that supplies culture-specific formatting information.</param>
         /// <param name="args">An object array that contains zero or more objects to format. </param>
         /// <returns>A copy of <paramref name="source"/> in which the format items have been replaced by the string representation of the corresponding objects in <paramref name="args"/>.</returns>
-        public static string FormatWith(this string source, IFormatProvider provider, params object[] args) {
+        public static string FormatWith(this string source, IFormatProvider provider, params object[] args)
+        {
             Guard.ArgumentNotNull("source", source);
 
             return String.Format(provider, source, args);
@@ -48,7 +52,7 @@
 
             if (endIndex <= startIndex || startIndex < 0)
                 return string.Empty;
-            
+
             return source.Substring(startIndex, endIndex - startIndex);
         }
     }
