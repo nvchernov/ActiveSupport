a7d0506f5ef7a7010a507d76e4fbc0cadca5eb09
nvchernov <q-cs@ya.ru>
2018-03-24 00:00:32 +0300

    integer methods refactoring

--- TypeSupport/IntegerEvenOdd.cs
+++ TypeSupport/IntegerEvenOdd.cs
@@ -1,14 +1,17 @@
-﻿namespace NSupport {
+﻿namespace NSupport
+{
     /// <summary>
     /// Provides extension methods for <see cref="int"/> for even/odds conversion.
     /// </summary>
-    public static class IntegerEvenOdd {
+    public static class IntegerEvenOdd
+    {
         /// <summary>
         /// Returns true if the number is odd, otherwise false.
         /// </summary>
         /// <param name="source">A <see cref="int"/> instance.</param>
         /// <returns>Returns true if the number is odd, otherwise false.</returns>
-        public static bool IsOdd(this int source) {
+        public static bool IsOdd(this int source)
+        {
             return source % 2 != 0;
         }
 
@@ -17,7 +20,8 @@
         /// </summary>
         /// <param name="source">A <see cref="int"/> instance.</param>
         /// <returns>Returns true if the number is even, otherwise false.</returns>        
-        public static bool IsEven(this int source) {
+        public static bool IsEven(this int source)
+        {
             return !source.IsOdd();
         }
 
@@ -27,7 +31,8 @@
         /// <param name="source">A <see cref="int"/> instance.</param>
         /// <param name="number">The multiplier.</param>
         /// <returns>Returns true if the number is multiple of <paramref name="number" />, otherwise false.</returns>        
-        public static bool IsMultipleOf(this int source, int number) {
+        public static bool IsMultipleOf(this int source, int number)
+        {
             return source % number == 0;
         }
     }
--- TypeSupport/IntegerTime.cs
+++ TypeSupport/IntegerTime.cs
@@ -1,17 +1,20 @@
-﻿namespace NSupport {
+﻿namespace NSupport
+{
     using System;
 
     /// <summary>
     /// Provides extension methods for <see cref="int"/> for <see cref="TimeSpan"/> conversion.
     /// </summary>
-    public static class IntegerTime {
+    public static class IntegerTime
+    {
         /// <summary>
         /// Returns a <see cref="TimeSpan"/> in second.
         /// </summary>
         /// <param name="source">A <see cref="int"/> instance.</param>
         /// <returns>Returns a <see cref="TimeSpan"/> in second.</returns>
-        public static TimeSpan Second(this int source) {
-            return source.Seconds();
+        public static TimeSpan Second(this int source)
+        {
+            return new TimeSpan(0, 0, source);
         }
 
         /// <summary>
@@ -19,7 +22,8 @@
         /// </summary>
         /// <param name="source">A <see cref="int"/> instance.</param>
         /// <returns>Returns a <see cref="TimeSpan"/> in seconds.</returns>
-        public static TimeSpan Seconds(this int source) {
+        public static TimeSpan Seconds(this int source)
+        {
             return new TimeSpan(0, 0, source);
         }
 
@@ -28,8 +32,9 @@
         /// </summary>
         /// <param name="source">A <see cref="int"/> instance.</param>
         /// <returns>Returns a <see cref="TimeSpan"/> in minute.</returns>
-        public static TimeSpan Minute(this int source) {
-            return source.Minutes();
+        public static TimeSpan Minute(this int source)
+        {
+            return new TimeSpan(0, source, 0);
         }
 
         /// <summary>
@@ -37,7 +42,8 @@
         /// </summary>
         /// <param name="source">A <see cref="int"/> instance.</param>
         /// <returns>Returns a <see cref="TimeSpan"/> in minutes.</returns>        
-        public static TimeSpan Minutes(this int source) {
+        public static TimeSpan Minutes(this int source)
+        {
             return new TimeSpan(0, source, 0);
         }
 
@@ -46,8 +52,9 @@
         /// </summary>
         /// <param name="source">A <see cref="int"/> instance.</param>
         /// <returns>Returns a <see cref="TimeSpan"/> in hour.</returns>        
-        public static TimeSpan Hour(this int source) {
-            return source.Hours();
+        public static TimeSpan Hour(this int source)
+        {
+            return new TimeSpan(source, 0, 0);
         }
 
         /// <summary>
@@ -55,18 +62,19 @@
         /// </summary>
         /// <param name="source">A <see cref="int"/> instance.</param>
         /// <returns>Returns a <see cref="TimeSpan"/> in hours.</returns>        
-        public static TimeSpan Hours(this int source) {
+        public static TimeSpan Hours(this int source)
+        {
             return new TimeSpan(source, 0, 0);
         }
-
 
         /// <summary>
         /// Returns a <see cref="TimeSpan"/> in day.
         /// </summary>
         /// <param name="source">A <see cref="int"/> instance.</param>
         /// <returns>Returns a <see cref="TimeSpan"/> in day.</returns>
-        public static TimeSpan Day(this int source) {
-            return source.Days();
+        public static TimeSpan Day(this int source)
+        {
+            return new TimeSpan(source, 0, 0, 0);
         }
 
         /// <summary>
@@ -74,7 +82,8 @@
         /// </summary>
         /// <param name="source">A <see cref="int"/> instance.</param>
         /// <returns>Returns a <see cref="TimeSpan"/> in days.</returns>
-        public static TimeSpan Days(this int source) {
+        public static TimeSpan Days(this int source)
+        {
             return new TimeSpan(source, 0, 0, 0);
         }
 
@@ -83,8 +92,9 @@
         /// </summary>
         /// <param name="source">A <see cref="int"/> instance.</param>
         /// <returns>Returns a <see cref="TimeSpan"/> in week.</returns>
-        public static TimeSpan Week(this int source) {
-            return source.Weeks();
+        public static TimeSpan Week(this int source)
+        {
+            return (source * 7).Days();
         }
 
         /// <summary>
@@ -92,44 +102,10 @@
         /// </summary>
         /// <param name="source">A <see cref="int"/> instance.</param>
         /// <returns>Returns a <see cref="TimeSpan"/> in weeks.</returns>
-        public static TimeSpan Weeks(this int source) {
+        public static TimeSpan Weeks(this int source)
+        {
             return (source * 7).Days();
         }
 
-        /// <summary>
-        /// Returns a <see cref="TimeSpan"/> in month.
-        /// </summary>
-        /// <param name="source">A <see cref="int"/> instance.</param>
-        /// <returns>Returns a <see cref="TimeSpan"/> in month.</returns>
-        public static TimeSpan Month(this int source) {
-            return source.Months();
-        }
-
-        /// <summary>
-        /// Returns a <see cref="TimeSpan"/> in months.
-        /// </summary>
-        /// <param name="source">A <see cref="int"/> instance.</param>
-        /// <returns>Returns a <see cref="TimeSpan"/> in months.</returns>        
-        public static TimeSpan Months(this int source) {
-            return (30 * source).Days();
-        }
-
-        /// <summary>
-        /// Returns a <see cref="TimeSpan"/> in year.
-        /// </summary>
-        /// <param name="source">A <see cref="int"/> instance.</param>
-        /// <returns>Returns a <see cref="TimeSpan"/> in year.</returns>
-        public static TimeSpan Year(this int source) {
-            return source.Years();
-        }
-
-        /// <summary>
-        /// Returns a <see cref="TimeSpan"/> in years.
-        /// </summary>
-        /// <param name="source">A <see cref="int"/> instance.</param>
-        /// <returns>Returns a <see cref="TimeSpan"/> in years.</returns>
-        public static TimeSpan Years(this int source) {
-            return (365 * source).Days() + (6 * source).Hours();
-        }
     }
 }
