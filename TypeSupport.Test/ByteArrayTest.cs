ce0ee7fbf45784bf78bfbad371cd1354b7360a2b
nvchernov <q-cs@ya.ru>
2018-09-17 23:41:36 +0300

    new methods to assign or get numbers from byte array

---
+++ TypeSupport.Test/ByteArrayTest.cs
@@ -0,0 +1,267 @@
+﻿namespace TypeSupport.Test
+{
+
+    using System;
+    using Xunit;
+    using TypeSupport;
+
+    public class AssignByteArrayTest
+    {
+        // ---------- TEST FOR INT16 ----------
+
+        [Fact]
+        public void Test_AssignInt16()
+        {
+            byte[] bytes = new byte[4];
+
+            bytes.AssignInt16(0x1011, 0);
+
+            Assert.Equal(bytes[0], 0x11);
+            Assert.Equal(bytes[1], 0x10);
+            Assert.Equal(bytes[2], 0);
+            Assert.Equal(bytes[3], 0);
+        }
+
+        [Fact]
+        public void Test_AssignUInt16()
+        {
+            byte[] bytes = new byte[4];
+
+            bytes.AssignInt16((ushort)0x1011, 0);
+
+            Assert.Equal(bytes[0], 0x11);
+            Assert.Equal(bytes[1], 0x10);
+            Assert.Equal(bytes[2], 0);
+            Assert.Equal(bytes[3], 0);
+        }
+
+        [Fact]
+        public void Test_AssignInt16_offset()
+        {
+            byte[] bytes = new byte[4];
+
+            bytes.AssignInt16(0x1011, 2);
+
+            Assert.Equal(bytes[0], 0);
+            Assert.Equal(bytes[1], 0);
+            Assert.Equal(bytes[2], 0x11);
+            Assert.Equal(bytes[3], 0x10);
+        }
+
+        [Fact]
+        public void Test_AssignInt16_offset_overflow()
+        {
+            byte[] bytes = new byte[4];
+
+            Assert.Throws<ArgumentException>(() => bytes.AssignInt16(0x1011, 3));
+            Assert.Throws<ArgumentException>(() => bytes.AssignInt16(0x1011, 3));
+
+            //TODO
+            //Assert.Throws<ArgumentException>(() => bytes.AssignInt16(0x5555, -1));
+        }
+
+        // ---------- TEST FOR INT32 ----------
+
+        [Fact]
+        public void Test_AssignInt32()
+        {
+            byte[] bytes = new byte[4];
+
+            bytes.AssignInt32(0x1011_1213, 0);
+
+            Assert.Equal(bytes[0], 0x13);
+            Assert.Equal(bytes[1], 0x12);
+            Assert.Equal(bytes[2], 0x11);
+            Assert.Equal(bytes[3], 0x10);
+        }
+
+        [Fact]
+        public void Test_AssignUInt32()
+        {
+            byte[] bytes = new byte[4];
+
+            bytes.AssignInt32((uint)0x1011_1213, 0);
+
+            Assert.Equal(bytes[0], 0x13);
+            Assert.Equal(bytes[1], 0x12);
+            Assert.Equal(bytes[2], 0x11);
+            Assert.Equal(bytes[3], 0x10);
+        }
+
+        [Fact]
+        public void Test_AssignInt32_offset()
+        {
+            byte[] bytes = new byte[8];
+
+            bytes.AssignInt32(0x1011_1213, 2);
+
+            Assert.Equal(bytes[0], 0);
+            Assert.Equal(bytes[1], 0);
+            Assert.Equal(bytes[2], 0x13);
+            Assert.Equal(bytes[3], 0x12);
+            Assert.Equal(bytes[4], 0x11);
+            Assert.Equal(bytes[5], 0x10);
+            Assert.Equal(bytes[6], 0);
+            Assert.Equal(bytes[7], 0);
+        }
+
+        [Fact]
+        public void Test_AssignInt32_offset_overflow()
+        {
+            byte[] bytes = new byte[8];
+
+            Assert.Throws<ArgumentException>(() => bytes.AssignInt32(0x1011_1213, 5));
+            Assert.Throws<ArgumentException>(() => bytes.AssignInt32(0x1011_1213, 6));
+
+            //TODO
+            //Assert.Throws<ArgumentException>(() => bytes.AssignInt16(0x5555, -1));
+        }
+
+        // ---------- TEST FOR INT64 ----------
+
+        [Fact]
+        public void Test_AssignInt64()
+        {
+            byte[] bytes = new byte[10];
+
+            bytes.AssignInt64(0x1011_1213_1415_1617, 0);
+
+            Assert.Equal(bytes[0], 0x17);
+            Assert.Equal(bytes[1], 0x16);
+            Assert.Equal(bytes[2], 0x15);
+            Assert.Equal(bytes[3], 0x14);
+            Assert.Equal(bytes[4], 0x13);
+            Assert.Equal(bytes[5], 0x12);
+            Assert.Equal(bytes[6], 0x11);
+            Assert.Equal(bytes[7], 0x10);
+            Assert.Equal(bytes[8], 0);
+            Assert.Equal(bytes[9], 0);
+
+        }
+
+        [Fact]
+        public void Test_AssignUInt64()
+        {
+            byte[] bytes = new byte[10];
+
+            bytes.AssignInt64((ulong)0x1011_1213_1415_1617, 0);
+
+            Assert.Equal(bytes[0], 0x17);
+            Assert.Equal(bytes[1], 0x16);
+            Assert.Equal(bytes[2], 0x15);
+            Assert.Equal(bytes[3], 0x14);
+            Assert.Equal(bytes[4], 0x13);
+            Assert.Equal(bytes[5], 0x12);
+            Assert.Equal(bytes[6], 0x11);
+            Assert.Equal(bytes[7], 0x10);
+            Assert.Equal(bytes[8], 0);
+            Assert.Equal(bytes[9], 0);
+        }
+
+        [Fact]
+        public void Test_AssignInt64_offset()
+        {
+            byte[] bytes = new byte[18];
+
+            bytes.AssignInt64(0x1011_1213_1415_1617, 2);
+
+            Assert.Equal(bytes[0], 0);
+            Assert.Equal(bytes[1], 0);
+            Assert.Equal(bytes[2], 0x17);
+            Assert.Equal(bytes[3], 0x16);
+            Assert.Equal(bytes[4], 0x15);
+            Assert.Equal(bytes[5], 0x14);
+            Assert.Equal(bytes[6], 0x13);
+            Assert.Equal(bytes[7], 0x12);
+            Assert.Equal(bytes[8], 0x11);
+            Assert.Equal(bytes[9], 0x10);
+            Assert.Equal(bytes[10], 0);
+            Assert.Equal(bytes[11], 0);
+        }
+
+        [Fact]
+        public void Test_AssignInt64_offset_overflow()
+        {
+            byte[] bytes = new byte[20];
+
+            byte[] oneElementByteArray = new byte[1];
+
+            Assert.Throws<ArgumentException>(() => bytes.AssignInt64(0x1011_1213_1415_1617, 16));
+            Assert.Throws<ArgumentException>(() => bytes.AssignInt64(0x1011_1213_1415_1617, 17));
+
+            Assert.Throws<ArgumentException>(() => oneElementByteArray.AssignInt64(0x55555555_55555555, 0));
+
+            //TODO
+            //Assert.Throws<ArgumentException>(() => bytes.AssignInt16(0x5555, -1));
+        }
+
+
+    }
+
+
+    public class GetIntFromByteArray
+    {
+        private byte[] m_Bytes = new byte[] { 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0, 0, 0, 0, 0, 0 };
+
+        [Fact]
+        public void Test_GetInt16()
+        {
+            Assert.Equal(0x1110, m_Bytes.GetInt16(0));
+        }
+
+        [Fact]
+        public void Test_GetInt16_with_offset()
+        {
+            Assert.Equal(0x1211, m_Bytes.GetInt16(1));
+        }
+
+        [Fact]
+        public void Test_GetInt16_offset_overflow()
+        {
+            Assert.Throws<ArgumentException>(() => m_Bytes.GetInt16(m_Bytes.Length - 1));
+        }
+
+
+
+        [Fact]
+        public void Test_GetInt32()
+        {
+            Assert.Equal(0x1312_1110, m_Bytes.GetInt32(0));
+        }
+
+        [Fact]
+        public void Test_GetInt32_with_offset()
+        {
+            Assert.Equal(0x1413_1211, m_Bytes.GetInt32(1));
+        }
+
+        [Fact]
+        public void Test_GetInt32_offset_overflow()
+        {
+            Assert.Throws<ArgumentException>(() => m_Bytes.GetInt32(m_Bytes.Length - 3));
+        }
+
+
+
+        [Fact]
+        public void Test_GetInt64()
+        {
+            Assert.Equal(0x1716_1514_1312_1110, m_Bytes.GetInt64(0));
+        }
+
+        [Fact]
+        public void Test_GetInt64_with_offset()
+        {
+            Assert.Equal(0x1817_1615_1413_1211, m_Bytes.GetInt64(1));
+        }
+
+        [Fact]
+        public void Test_GetInt64_offset_overflow()
+        {
+            Assert.Throws<ArgumentException>(() => m_Bytes.GetInt64(m_Bytes.Length - 4));
+        }
+
+
+
+    }
+}
---
+++ TypeSupport/ByteArray.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace TypeSupport
+{
+    public static class ByteArray
+    {
+        private const string BYTE_ARRAY_TOO_SMALL_EXCEPTION_MESSAGE = "Byte array has not enough elements to assign Int32";
+
+        private const string BYTE_ARRAY_CANNOT_BE_NULL_EXCEPTION_MESSAGE = "Byte array cannot be null";
+
+        /// <summary>
+        /// Assign <see cref="Int16"/> to array by specified offset
+        /// </summary>
+        /// <param name="source">Source byte array</param>
+        /// <param name="value">Value to assign</param>
+        /// <param name="offset">Byte array offset</param>
+        public static void AssignInt16(this byte[] source, short value, int offset)
+        {
+            if (source == null)
+                throw new ArgumentNullException(BYTE_ARRAY_CANNOT_BE_NULL_EXCEPTION_MESSAGE);
+
+            if (source.Length - offset < 2)
+                throw new ArgumentException(BYTE_ARRAY_TOO_SMALL_EXCEPTION_MESSAGE);
+
+            var union = new Type2ByteUnion();
+
+            union.Value = value;
+
+            source[offset] = union.b0;
+            source[offset + 1] = union.b1;
+        }
+
+        /// <summary>
+        /// Assign <see cref="UInt16"/> to array by specified offset
+        /// </summary>
+        /// <param name="source">Source byte array</param>
+        /// <param name="value">Value to assign</param>
+        /// <param name="offset">Byte array offset</param>
+        public static void AssignInt16(this byte[] source, ushort value, int offset) =>
+             source.AssignInt16(unchecked((short)value), offset);
+
+        /// <summary>
+        /// Assign <see cref="Int32"/> to array by specified offset
+        /// </summary>
+        /// <param name="source">Source byte array</param>
+        /// <param name="value">Value to assign</param>
+        /// <param name="offset">Byte array offset</param>
+        public static void AssignInt32(this byte[] source, int value, int offset)
+        {
+            if (source == null)
+                throw new ArgumentNullException(BYTE_ARRAY_CANNOT_BE_NULL_EXCEPTION_MESSAGE);
+
+            if (source.Length - offset < 4)
+                throw new ArgumentException(BYTE_ARRAY_TOO_SMALL_EXCEPTION_MESSAGE);
+
+            var union = new Type4ByteUnion();
+
+            union.Value = value;
+
+            source[offset] = union.b0;
+            source[offset + 1] = union.b1;
+            source[offset + 2] = union.b2;
+            source[offset + 3] = union.b3;
+        }
+
+        /// <summary>
+        /// Assign <see cref="UInt32"/> to array by specified offset
+        /// </summary>
+        /// <param name="source">Source byte array</param>
+        /// <param name="value">Value to assign</param>
+        /// <param name="offset">Byte array offset</param>
+        public static void AssignInt32(this byte[] source, uint value, int offset) =>
+            source.AssignInt32(unchecked((int)value), offset);
+
+        /// <summary>
+        /// Assign <see cref="UInt64"/> to array by specified offset
+        /// </summary>
+        /// <param name="source">Source byte array</param>
+        /// <param name="value">Value to assign</param>
+        /// <param name="offset">Byte array offset</param>
+        public static void AssignInt64(this byte[] source, long value, int offset)
+        {
+            if (source == null)
+                throw new ArgumentNullException(BYTE_ARRAY_CANNOT_BE_NULL_EXCEPTION_MESSAGE);
+
+            if (source.Length - offset < 8)
+                throw new ArgumentException(BYTE_ARRAY_TOO_SMALL_EXCEPTION_MESSAGE);
+
+            var union = new Type8ByteUnion();
+
+            union.Value = value;
+
+            source[offset] = union.b0;
+            source[offset + 1] = union.b1;
+            source[offset + 2] = union.b2;
+            source[offset + 3] = union.b3;
+            source[offset + 4] = union.b4;
+            source[offset + 5] = union.b5;
+            source[offset + 6] = union.b6;
+            source[offset + 7] = union.b7;
+        }
+
+        /// <summary>
+        /// Assign <see cref="UInt64"/> to array by specified offset
+        /// </summary>
+        /// <param name="source">Source byte array</param>
+        /// <param name="value">Value to assign</param>
+        /// <param name="offset">Byte array offset</param>
+        public static void AssignInt64(this byte[] source, ulong value, int offset) =>
+            source.AssignInt64(unchecked((long)value), offset);
+
+
+        /// <summary>
+        /// Get <see cref="Int16"/> from array by array offset
+        /// </summary>
+        /// <param name="source">Source byte array</param>
+        /// <param name="offset">Byte array offset</param>
+        /// <returns>Returns value converted from byte array</returns>
+        public static short GetInt16(this byte[] source, int offset)
+        {
+            if (source == null)
+                throw new ArgumentNullException(BYTE_ARRAY_CANNOT_BE_NULL_EXCEPTION_MESSAGE);
+
+            if (source.Length - offset < 2)
+                throw new ArgumentException(BYTE_ARRAY_TOO_SMALL_EXCEPTION_MESSAGE);
+
+            var union = new Type2ByteUnion();
+
+            union.b0 = source[offset];
+            union.b1 = source[offset + 1];
+
+            return union.Value;
+        }
+
+        /// <summary>
+        /// Get <see cref="UInt16"/> from array by array offset
+        /// </summary>
+        /// <param name="source">Source byte array</param>
+        /// <param name="offset">Byte array offset</param>
+        /// <returns>Returns value converted from byte array</returns>
+        public static ushort GetUInt16(this byte[] source, int offset) =>
+            unchecked((ushort)source.GetInt16(offset));
+
+        /// <summary>
+        /// Get <see cref="Int32"/> from array by array offset
+        /// </summary>
+        /// <param name="source">Source byte array</param>
+        /// <param name="offset">Byte array offset</param>
+        /// <returns>Returns value converted from byte array</returns>
+        public static int GetInt32(this byte[] source, int offset)
+        {
+            if (source == null)
+                throw new ArgumentNullException(BYTE_ARRAY_CANNOT_BE_NULL_EXCEPTION_MESSAGE);
+
+            if (source.Length - offset < 4)
+                throw new ArgumentException(BYTE_ARRAY_TOO_SMALL_EXCEPTION_MESSAGE);
+
+            var union = new Type4ByteUnion();
+
+            union.b0 = source[offset];
+            union.b1 = source[offset + 1];
+            union.b2 = source[offset + 2];
+            union.b3 = source[offset + 3];
+
+            return union.Value;
+        }
+
+        /// <summary>
+        /// Get <see cref="UInt32"/> from array by array offset
+        /// </summary>
+        /// <param name="source">Source byte array</param>
+        /// <param name="offset">Byte array offset</param>
+        /// <returns>Returns value converted from byte array</returns>
+        public static uint GetUInt32(this byte[] source, int offset) =>
+            unchecked((uint)source.GetInt32(offset));
+
+        /// <summary>
+        /// Get <see cref="Int64"/> from array by array offset
+        /// </summary>
+        /// <param name="source">Source byte array</param>
+        /// <param name="offset">Byte array offset</param>
+        /// <returns>Returns value converted from byte array</returns>
+        public static long GetInt64(this byte[] source, int offset)
+        {
+            if (source == null)
+                throw new ArgumentNullException(BYTE_ARRAY_CANNOT_BE_NULL_EXCEPTION_MESSAGE);
+
+            if (source.Length - offset < 8)
+                throw new ArgumentException(BYTE_ARRAY_TOO_SMALL_EXCEPTION_MESSAGE);
+
+            var union = new Type8ByteUnion();
+
+            union.b0 = source[offset];
+            union.b1 = source[offset + 1];
+            union.b2 = source[offset + 2];
+            union.b3 = source[offset + 3];
+            union.b4 = source[offset + 4];
+            union.b5 = source[offset + 5];
+            union.b6 = source[offset + 6];
+            union.b7 = source[offset + 7];
+
+            return union.Value;
+        }
+
+        /// <summary>
+        /// Get <see cref="UInt64"/> from array by array offset
+        /// </summary>
+        /// <param name="source">Source byte array</param>
+        /// <param name="offset">Byte array offset</param>
+        /// <returns>Returns value converted from byte array</returns>
+        public static ulong GetUInt64(this byte[] source, int offset) =>
+            unchecked((uint)source.GetInt64(offset));
+
+    }
+
+    [StructLayout(LayoutKind.Explicit)]
+    internal struct Type2ByteUnion
+    {
+        [FieldOffset(0)]
+        public short Value;
+
+        [FieldOffset(0)]
+        public byte b0;
+
+        [FieldOffset(1)]
+        public byte b1;
+    }
+
+    [StructLayout(LayoutKind.Explicit)]
+    internal struct Type4ByteUnion
+    {
+        [FieldOffset(0)]
+        public int Value;
+
+        [FieldOffset(0)]
+        public byte b0;
+
+        [FieldOffset(1)]
+        public byte b1;
+
+        [FieldOffset(2)]
+        public byte b2;
+
+        [FieldOffset(3)]
+        public byte b3;
+    }
+
+
+    [StructLayout(LayoutKind.Explicit)]
+    internal struct Type8ByteUnion
+    {
+        [FieldOffset(0)]
+        public long Value;
+
+        [FieldOffset(0)]
+        public byte b0;
+
+        [FieldOffset(1)]
+        public byte b1;
+
+        [FieldOffset(2)]
+        public byte b2;
+
+        [FieldOffset(3)]
+        public byte b3;
+
+        [FieldOffset(4)]
+        public byte b4;
+
+        [FieldOffset(5)]
+        public byte b5;
+
+        [FieldOffset(6)]
+        public byte b6;
+
+        [FieldOffset(7)]
+        public byte b7;
+    }
+
+}
